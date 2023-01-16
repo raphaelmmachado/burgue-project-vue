072e18bb5fcccf991256c8a5fcc9e260043f26f7
Daniel <danieldossantosdacosta@gmail.com>
2023-01-15 21:34:34 -0300

    add base project and config

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+export default { name: "Navbar" };
+</script>
+<template>
+  <div id="nav">
+    <nav>
+      <RouterLink to="/">
+        <img src="" alt="" id="logo" />
+      </RouterLink>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+
+  <RouterView />
+</template>
+<style scoped></style>
