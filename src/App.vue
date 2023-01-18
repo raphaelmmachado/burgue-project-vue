7e686362c78963a1b073f76fdacb4a921326b770
Daniel <danieldossantosdacosta@gmail.com>
2023-01-17 21:08:13 -0300

    add footer on main app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
+<template>
+  <Navbar />
+  <Footer />
+</template>
+
 <script>
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
 export default {
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
-<template>
-  <Navbar />
-</template>
 <style scoped></style>
