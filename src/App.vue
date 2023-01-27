65f52c15e5de22252d328829673963837a07b68b
Daniel <danieldossantosdacosta@gmail.com>
2023-01-26 21:49:11 -0300

    add props and add style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar />
+  <Navbar :logo="logo_src" :alt="app_name" />
   <Footer />
 </template>
 
@@ -12,6 +12,29 @@ export default {
     Navbar,
     Footer,
   },
+  data() {
+    return {
+      logo_src: "img/logo.png ",
+      app_name: "Make Your Burger",
+    };
+  },
 };
 </script>
-<style scoped></style>
+<style>
+* {
+  font-family: Helvetica, sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+</style>
