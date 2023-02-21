2a5ee82f5ae4a38e448c4c138315590a615bb410
Daniel Costa <danieldossantosdacosta@gmail.com>
2023-02-20 23:08:56 -0300

    add form(i need to finished)

---
+++ src/components/BurguerForm.vue
@@ -0,0 +1,36 @@
+<template>
+   <div>
+    <p>Componente de mensagem</p>
+    <div>
+        <form  id="burguer-form">
+        <div class="input-container">
+        <label for="name">Nome do cliente:</label>
+        <input type="text" name="name" id="name" v-model="name" placeholder="Digite seu nome!">
+    </div>
+     <div class="input-container">
+        <label for="bread">Escolha o pão:</label>
+        <select name="bread" id="bread" v-model="bread">
+        <option value="">Selecione o seu pão</option>
+        <option value="integral">integral</option>
+    </select>        
+    </div>
+    <div class="input-container">
+        <label for="meat">Escolha a carne:</label>
+        <select name="meat" id="meat" v-model="meat">
+        <option value="">Selecione o tipo de carne</option>
+        <option value="maminha">Maminha</option>
+    </select>        
+    </div>
+        </form>
+</div>
+   </div>
+</template>
+<script>
+    export default {
+        name: "BurguerForm"
+
+    }
+</script>
+<style scoped>
+
+</style>
